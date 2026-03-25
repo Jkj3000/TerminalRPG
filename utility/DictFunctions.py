f30aa27e6e7151597c0e06c6f5e8c75c093a2b60
Jkj3000 <Jkj3000@205751113+Jkj3000@users.noreply.github.com>
2026-03-25 14:40:39 +0100

    Added Dictionary functions

---
+++ utility/DictFunctions.py
@@ -0,0 +1,11 @@
+from beautifultable import BeautifulTable
+from termcolor import colored
+
+def makeDictionaryTable(dictionary):
+    table = BeautifulTable()
+    table.columns.header = ["Stat", "Value"]
+    for key, value in dictionary.items():
+        table.rows.append([key, colored(value, "red")])
+    return table
+
+
